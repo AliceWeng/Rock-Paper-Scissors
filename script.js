5e4648485671acada6f4d9d324b888fe2529c895
Alice <alicewengs@gmail.com>
2022-11-04 09:08:20 -0700

    added functions to create buttons and headings, also a button clicking sound effect

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 let game = document.getElementById("game");
 let start = document.getElementById("start");
 
+let createButton = rps => {
+    let button = document.createElement("button");
+    button.textContent = rps;
+    button.className = "rpsButton"
+    button.id = rps;
+    game.append(button)
+}
+let createHeading = string => {
+    let h1 = document.createElement("h1");
+    h1.textContent = string;
+    h1.className = "heading";
+    game.append(h1);
+}
+
+let click = new Audio("./img/click.mp3");
+
 start.addEventListener("click", () => {
+    click.play();
     setTimeout(() => {
         start.remove();
+        createHeading("choose your fighter");
+        createButton("rock");
+        createButton("paper");
+        createButton("scissors");
     }, 300);
 })
